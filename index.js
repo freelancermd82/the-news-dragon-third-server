c8a994651f0f573704b50bdd4c02c766dc17303d
Your Name <freelancermd82@gmail.com>
2024-08-28 23:02:32 +0600

    category news _id id

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('Dragon is coming');
 });
 
@@ -15,9 +16,31 @@ app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
+app.get('/news', (req, res) => {
+    res.send(news);
+})
 
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
 
-app.listen(port, ()=>{
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews);
+    }
+
+})
+
+
+app.listen(port, () => {
     console.log(`explore app listing on port: ${port}`)
 });
